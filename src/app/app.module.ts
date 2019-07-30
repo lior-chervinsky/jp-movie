7085a0f9534d18f106c862a10d378f00db1773de
Chervinsky Lior <l.chervinsky@f5.com>
2019-07-30 16:34:08 +0300

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AkitaNgDevtools} from '@datorama/akita-ngdevtools';
+import {AkitaNgRouterStoreModule} from '@datorama/akita-ng-router-store';
+import {environment} from '../environments/environment';
+import {MovieListComponent} from './movie-list/movie-list.component';
+import {ListItemComponent} from './movie-list/list-item/list-item.component';
+import {ColorPatchComponent} from './movie-list/color-patch/color-patch.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SharedModule} from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    SharedModule,
+    environment.production ? [] : AkitaNgDevtools.forRoot(),
+    AkitaNgRouterStoreModule.forRoot()
+  ],
+  declarations: [
+    AppComponent,
+    MovieListComponent,
+    ListItemComponent,
+    ColorPatchComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
